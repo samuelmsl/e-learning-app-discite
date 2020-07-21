0279c6370d11297900f5aafc06bd494c29838032
jansenmanuel <jansen.manuel@nexsoft.co.id>
2020-07-21 10:55:24 +0700

    testing

--- elearning-react/src/Components/Register.js
+++ elearning-react/src/Components/Register.js
@@ -2,8 +2,55 @@ import React, { Component } from 'react'
 import registerImage from '../Assets/Images/registersvg.svg'
 import './Register.css'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 export default class Register extends Component {
+    state = {
+        nama: '',
+        pasword: '',
+        type: 'siswa',
+        username: '',
+        id_kelas: '',
+    }
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const user = {
+            nama: this.state.nama,
+            password: this.state.password,
+            type: this.state.type,
+            username: this.state.username,
+            kelas: {
+                id: parseInt(this.state.id_kelas)
+            }
+        };
+
+        console.log(user)
+
+        // axios.post("http://localhost:8080/public/register", { user })
+        //     .then((res) => console.log(res))
+        //     .catch((err) => console.log(err));
+
+        fetch(`http://localhost:8080/public/register`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, /',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(response => {
+                console.log(response)
+            })
+            .catch(console.error);
+    }
+
     render() {
         return (
             <>
@@ -19,16 +66,13 @@ export default class Register extends Component {
                                     <img src={registerImage} alt="" className="register-img mt-5" />
                                 </div>
                                 <div className="col-sm-4">
-                                    <form className="text-light">
-                                        <h3>Register Page</h3>
-                                        <input type="text" className="form-control my-3" name="nama" id="nama" placeholder="Nama" maxLength="30" autoComplete="off" required />
-                                        <input type="text" className="form-control my-3" name="username" id="username" placeholder="Username" maxLength="30" autoComplete="off" required />
-                                        <input type="email" className="form-control my-3" name="email" id="email" placeholder="Email" maxLength="40" autoComplete="off" required />
-                                        <input type="password" className="form-control my-3" name="password" id="username" maxLength="30" placeholder="Password" required />
-                                        <input type="text" className="form-control my-3" name="kelas" id="kelas" placeholder="Kelas" maxLength="20" autoComplete="off" required />
-                                        <button type="submit" className="btn register-btn px-3 mb-3">Register</button>
-                                        <p>Sudah punya akun? <Link to="/login" className="text-light">Login</Link> </p>
-                                    </form>
+                                    <h3>Register Page</h3>
+                                    <input type="text" className="form-control my-3" name="nama" id="nama" placeholder="Nama" maxLength="30" autoComplete="off" required onChange={this.handleChange} />
+                                    <input type="text" className="form-control my-3" name="username" id="username" placeholder="Username" maxLength="30" autoComplete="off" required onChange={this.handleChange} />
+                                    <input type="password" className="form-control my-3" name="password" id="password" maxLength="30" placeholder="Password" required onChange={this.handleChange} />
+                                    <input type="text" className="form-control my-3" name="id_kelas" id="id_kelas" placeholder="Kelas" maxLength="20" autoComplete="off" required onChange={this.handleChange} />
+                                    <button type="submit" className="btn register-btn px-3 mb-3" onClick={this.handleSubmit}>Register</button>
+                                    <p>Sudah punya akun? <Link to="/login" className="text-light">Login</Link> </p>
                                 </div>
                             </div>
                         </div>

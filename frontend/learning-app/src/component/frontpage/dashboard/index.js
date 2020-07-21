0ef463cfb1854b1367698fb473ed55d447fdbeaf
samuelmsl <smarsellino@gmail.com>
2020-07-21 08:48:46 +0700

    frontpage

--- frontend/learning-app/src/component/frontpage/dashboard/index.js
+++ frontend/learning-app/src/component/frontpage/dashboard/index.js
@@ -1,15 +1,42 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Index from '../../img/index.png';
+import "./app.css";
 
 class Dashboard extends Component {
-    render() { 
-        return ( 
+    render() {
+        return (
 
             <>
-            Hai ini guru
+                <div className="text-light">
+                    <div className=" ml-4 mt-2 navbar text-light"><h1 className="font-weight-bold">Discite</h1></div>
+                    <div className="ml-5 mt-4">
+                        <h3 className="ml-5 mt-5">“Lorem ipsum dolor sit amet<br></br>
+                        consectetur adipiscing elit<br></br>
+                        sed do eiusmod”
+                        </h3>
+                        <h1 className="font-weight-bold mt-5 ml-5">
+                            Tetap Belajar walaupun <br></br>dirumah aja..
+                        </h1>
+
+                        <div className="buttonlogin ml-5 mt-4 bluetext d-flex justify-content-center align-items-center">
+                            <a href = "/register">
+                                Daftar Sekarang
+                                </a>
+                    </div>
+                    <h3 className = "ml-5 mt-4">Sudah memiliki akun?</h3>
+                    
+                    <div className="buttonlogin ml-5 mt-4 bluetext d-flex justify-content-center align-items-center">
+                          <a href = "/login">
+                               Masuk
+                              </a> 
+                    </div>
+                    </div>
+                </div>
+
             </>
 
-         );
+        );
     }
 }
- 
+
 export default Dashboard;
